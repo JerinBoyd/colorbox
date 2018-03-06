a85e22a92f70195f433634b7c47c55dd72b50650
JerinBoyd <maxers@mac.com>
2018-03-06 18:04:04 -0500

    initial setup for colorpicker

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css/';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello
+    
+        
+      </div>
+    );
+  }
+}
+
+export default App;
